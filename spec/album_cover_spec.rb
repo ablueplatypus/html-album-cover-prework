b9aecf00354c31fcca820e207fda771cf8e3e5dd
Matt Doering <mattdoering86@gmail.com>
2018-11-16 01:46:22 +0000

    automatically backed up by learn

--- spec/album_cover_spec.rb
+++ spec/album_cover_spec.rb
@@ -1,12 +1,20 @@
 RSpec.describe 'HTML Album Cover' do
 
     context 'within <body>' do
+      it 'the title-box section contains the correct header sizes and text content' do
+        title_box = parsed_html.search('#title-box').first
 
         expect(title_box.children.any? {|ch| ch.name == "h4"}).to be == true, "No 'h4' tag(s) found"
         expect(title_box.children.select {|ch| ch.name == "h4"}.first.children.first.text).to be == "PHARRELL WILLIAMS", "Your 'h4' tag should contain the text 'PHARRELL WILLIAMS'"
 
         expect(title_box.children.any? {|ch| ch.name == "h2"}).to be == true, "No 'h2' tag found"
         expect(title_box.children.select {|ch| ch.name == "h2"}.first.children.first.text).to be == "HAPPY", "Your 'h2' tag should contain the text 'HAPPY'"
+
+      end
+
+
+      it 'the back section contains the correct header sizes and text content' do
+        back_cover = parsed_html.search('#back').first
 
         expect(back_cover.children.any? {|ch| ch.name == "h1"}).to be == true, "No 'h1' tag found"
         expect(back_cover.children.select {|ch| ch.name == "h1"}.first.children.first.text).to be == "HAPPY", "The `h1` tag should include the text 'HAPPY'"
@@ -23,4 +31,5 @@ RSpec.describe 'HTML Album Cover' do
 
       end
 
+  end
 end
