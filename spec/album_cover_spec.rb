8f8b8a1c5bf3d6bad4d8da4fe9f477abc5189e52
Matt Doering <mattdoering86@gmail.com>
2018-11-16 01:36:18 +0000

    automatically backed up by learn

--- spec/album_cover_spec.rb
+++ spec/album_cover_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe 'HTML Album Cover' do
 
     context 'within <body>' do
       it 'the title-box section contains the correct header sizes and text content' do
-        title_box = parsed_html.search('#title-box').first
+        title_box = parsed_html.search('title-box').first
 
         expect(title_box.children.any? {|ch| ch.name == "h4"}).to be == true, "No 'h4' tag(s) found"
         expect(title_box.children.select {|ch| ch.name == "h4"}.first.children.first.text).to be == "PHARRELL WILLIAMS", "Your 'h4' tag should contain the text 'PHARRELL WILLIAMS'"
